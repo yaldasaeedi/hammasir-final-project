543ba2a6d2ce2d907aa07397d989a996c6566eeb
yaldasaeedi <yalda.saeedi1381@gmail.com>
2023-09-12 23:58:42 +0330

    Initial Commit

---
+++ hammasir-final-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hammasir-final-project
+//
+//  Created by Helen Besharatian on 6/21/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hammasir-final-projectUITests/hammasir_final_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hammasir_final_projectUITestsLaunchTests.swift
+//  hammasir-final-projectUITests
+//
+//  Created by Helen Besharatian on 6/21/1402 AP.
+//
+
+import XCTest
+
+final class hammasir_final_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
