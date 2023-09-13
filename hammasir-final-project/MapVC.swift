70cb8ae63ebc2b37c456f045028b021cd28f13d0
yaldasaeedi <yalda.saeedi1381@gmail.com>
2023-09-13 13:40:28 +0330

    WIP: working on map Model. added the Notification class

--- hammasir-final-project/MapVC.swift
+++ hammasir-final-project/MapVC.swift
@@ -8,41 +8,24 @@
 import UIKit
 
 class MapVC: UIViewController {
+    var MapModel : MapM?
     var mapview: NTMapView?
     @IBOutlet weak var mapUIV: UIView!
     @IBOutlet weak var SearchDestinationUISB: UISearchBar!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadMap()
         
-}
-
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func showYesNoNotification() {
-            let alertController = UIAlertController(title: "Notification", message: "Do you want to add your companies?", preferredStyle: .alert)
-            
-            let yesAction = UIAlertAction(title: "Yes", style: .default) { (_) in
-                // Handle "Yes" button action you should pass the view to travelVC
-                
-                
-            }
-            
-            let noAction = UIAlertAction(title: "No", style: .cancel) { (_) in
-                // Handle "No" button action
-                
-            }
-            
-            alertController.addAction(yesAction)
-            alertController.addAction(noAction)
-            
-            present(alertController, animated: true, completion: nil)
-    }
-        
+    
+    
     @IBAction func searchingForDestination(_ sender: Any) {
         
         
@@ -55,26 +38,23 @@ class MapVC: UIViewController {
             print("Map does not load")
             return
         }
-
-        let neshan = NTNeshanServices.createBaseMap(NTNeshanMapStyle.NESHAN)
-        unwrappedMapView.getLayers().add(neshan)
-
-        let neshan2 = NTNeshanServices.createTrafficLayer()
-        unwrappedMapView.getLayers().add(neshan2)
-
-        let neshan3 = NTNeshanServices.createPOILayer(false)
-        unwrappedMapView.getLayers().add(neshan3)
-
-        unwrappedMapView.setFocalPointPosition(NTLngLat(x: 59.2, y: 36.5), durationSeconds: 0.4)
-        unwrappedMapView.setZoom(13, durationSeconds: 0.4)
-
-        mapUIV.addSubview(unwrappedMapView)
+        let returnedMapView = MapModel?.loadMap(mapView: unwrappedMapView)
+        mapUIV.addSubview(returnedMapView!)
     }
     
-    
-        
+    func longPressOnMap (){
+                // Add a long press gesture recognizer to the view
+            let longPressGesture = UILongPressGestureRecognizer(target: self, action:#selector(viewLongPressed(_:)))
+            mapUIV.addGestureRecognizer(longPressGesture)
+    }
+            
+    @objc func viewLongPressed(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            print("View long-pressed!")
+                    // Perform any desired actions here
+            }
+        }
 }
-
     /*
     // MARK: - Navigation
 
